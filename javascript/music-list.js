318b1c76fbc83ac00933ac4c78f41ed5285b7d28
Denilson <129228213+Deunisso@users.noreply.github.com>
2023-08-29 22:01:24 -0300

    Add files via upload

---
+++ javascript/music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [
+    {
+        name: "Maniac",
+        artist: "Conan Gray",
+        img: "Maniac",
+        src: "Maniac"
+    },
+    {
+        name: "One Last Time",
+        artist: "Ariana Grande (Já tinha ouvido)",
+        img: "One Last Time",
+        src: "One Last Time"
+    },
+    {
+        name: "Seven",
+        artist: "Jung Kook",
+        img: "Seven",
+        src: "Seven"
+    },
+    {
+        name: "Leão",
+        artist: "Marília Mendonça (Já tinha ouvido)",
+        img: "Leão",
+        src: "Leão"
+    },
+    {
+        name: "Smoke Sprite",
+        artist: "RM (Só por causa do solo da guitarra kkkk')",
+        img: "Smoke Sprite",
+        src: "Smoke Sprite"
+    },
+    {
+        name: "Locked Out of Heaven",
+        artist: "Bruno Mars (Já tinha ouvido)",
+        img: "Locked Out of Heaven",
+        src: "Locked Out of Heaven"
+    },
+    {
+        name: "Tove Lo",
+        artist: "Habits (Já tinha ouvido)",
+        img: "Tove Lo",
+        src: "Tove Lo"
+    },
+    {
+        name: "Angel Pt. 1",
+        artist: "Everybody",
+        img: "Angel Pt. 1",
+        src: "Angel Pt. 1"
+    },
+    {
+        name: "That’s What I Like",
+        artist: "Bruno Mars (Já tinha ouvido)",
+        img: "That’s What I Like",
+        src: "That’s What I Like"
+    },
+    {
+        name: "Golden",
+        artist: "Harry Styles",
+        img: "Golden",
+        src: "Golden"
+    },
+]
